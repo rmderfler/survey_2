5928d7ef0f53ff35c19ab94b865e409d8b0151ca
Sergii Mertsalov & Michael Derfler <student@epicodus.com>
2014-08-14 10:18:29 -0700

    set up folder and file structure

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'active_record'
+require 'rspec'
+require 'survey'
+require 'question'
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))["test"])
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Survey.all.each { |survey| survey.destroy }
+    Question.all.each { |question| question.destroy }
+  end
+end
